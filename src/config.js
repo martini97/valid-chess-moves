7adc1d6f1793dd5b7b2e437ba4e8da2f3dc7315c
Alessandro Martini <alessandro.martini@bravi.com.br>
2018-07-14 03:04:19 -0300

    Fix heroku deploy
    We cannot set the host, so let heroku manage it.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 export default {
   http: {
-    host: process.env.HOST || '127.0.0.1',
     port: process.env.PORT || '8080',
   },
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import app from './app';
 import config from './config';
 
-app.listen(config.http.port, config.http.host, () => {
-  /* eslint no-console:0 */
-  console.info('Server started at [ http://%s:%s ]', config.http.host, config.http.port);
-});
+/* eslint no-console:0 */
+app.listen(config.http.port, () => console.log(`Listening on ${config.http.port}`));
